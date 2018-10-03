40ab1ee0d5d57296ecda3528b8b4f77f70fa12d5
Julianna Tetreault <juliannatetreault@icloud.com>
2018-10-03 07:14:52 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,9 +2,9 @@
 class Anagram 
   attr_accessor :word 
 
-  #def initialize 
-  #  @word = word
-  #end
+  def initialize 
+    @word = word
+  end
   
   def match(words) 
     if %w(words).select{ |w| w.words == words}
